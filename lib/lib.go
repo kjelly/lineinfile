af011cce6182cc1098a6a274e091da13c190c69c
Kuo-tung Kao <ya790206@gmail.com>
2018-06-11 22:55:28 +0800

    add more feature
    support --startline, --endline, --beforepattern, --afterpattern

--- lib/lib.go
+++ lib/lib.go
@@ -1,14 +1,46 @@
 package lib
 
 import (
+	_ "fmt"
 	"regexp"
 	"strings"
 )
 
+func InitTextProcessor(startLine int, endLine int, beforePattern string,
+	afterPattern string) (*TextProcessor, error) {
+	re1, err := regexp.Compile(beforePattern)
+	if err != nil {
+		return nil, err
+	}
+	re2, err := regexp.Compile(afterPattern)
+	if err != nil {
+		return nil, err
+	}
+	t := &TextProcessor{
+		StartLine:          startLine,
+		EndLine:            endLine,
+		BeforePattern:      re1,
+		AfterPattern:       re2,
+		inSearchArea:       false,
+		lastSearchAreaLine: -1,
+	}
+	return t, nil
+
+}
+
+type TextProcessor struct {
+	StartLine          int
+	EndLine            int
+	BeforePattern      *regexp.Regexp
+	AfterPattern       *regexp.Regexp
+	inSearchArea       bool
+	lastSearchAreaLine int
+}
+
 // Replace replace pattern with text
-func Replace(lines []string, re *regexp.Regexp, text string) []string {
+func (t TextProcessor) Replace(lines []string, re *regexp.Regexp, text string) []string {
 	var outputs []string
-	outputs = handleLines(lines, re, func(l string, m []string, o []string, re *regexp.Regexp) []string {
+	outputs = t.handleLines(lines, re, func(l string, m []string, o []string, re *regexp.Regexp) []string {
 		return append(o, strings.Replace(l, m[0], text, -1))
 
 	}, nil)
@@ -16,9 +48,9 @@ func Replace(lines []string, re *regexp.Regexp, text string) []string {
 }
 
 // InsertAfter insert text after pattern.
-func InsertAfter(lines []string, re *regexp.Regexp, text string) []string {
+func (t TextProcessor) InsertAfter(lines []string, re *regexp.Regexp, text string) []string {
 	var outputs []string
-	outputs = handleLines(lines, re, func(l string, m []string, o []string, re *regexp.Regexp) []string {
+	outputs = t.handleLines(lines, re, func(l string, m []string, o []string, re *regexp.Regexp) []string {
 		return append(o, strings.Replace(l, m[0], m[0]+text, -1))
 
 	}, nil)
@@ -26,9 +58,9 @@ func InsertAfter(lines []string, re *regexp.Regexp, text string) []string {
 }
 
 // InsertBefore insert text before pattern.
-func InsertBefore(lines []string, re *regexp.Regexp, text string) []string {
+func (t TextProcessor) InsertBefore(lines []string, re *regexp.Regexp, text string) []string {
 	var outputs []string
-	outputs = handleLines(lines, re, func(l string, m []string, o []string, re *regexp.Regexp) []string {
+	outputs = t.handleLines(lines, re, func(l string, m []string, o []string, re *regexp.Regexp) []string {
 		return append(o, strings.Replace(l, m[0], text+m[0], -1))
 
 	}, nil)
@@ -36,45 +68,95 @@ func InsertBefore(lines []string, re *regexp.Regexp, text string) []string {
 }
 
 // Absent remove pattern if found.
-func Absent(lines []string, re *regexp.Regexp) []string {
+func (t TextProcessor) Absent(lines []string, re *regexp.Regexp) []string {
 	var outputs []string
-	outputs = handleLines(lines, re, func(l string, m []string, o []string, re *regexp.Regexp) []string {
+	outputs = t.handleLines(lines, re, func(l string, m []string, o []string, re *regexp.Regexp) []string {
 		return o
 	}, nil)
 	return outputs
 }
 
 // Present make sure pattern exists.
-func Present(lines []string, re *regexp.Regexp, pattern string, text string) []string {
+func (t TextProcessor) Present(lines []string, re *regexp.Regexp, pattern string, text string) []string {
 	var outputs []string
-	outputs = handleLines(lines, re, nil, func(o []string, re *regexp.Regexp) []string {
+	outputs = t.handleLines(lines, re, nil, func(o []string, re *regexp.Regexp, p int) []string {
 		if text == "" {
-			return append(o, pattern)
+			return insertIntoLines(p, pattern, o)
 		}
-		return append(o, text)
+		return insertIntoLines(p, text, o)
 	})
 	return outputs
 }
 
-func handleLines(lines []string, re *regexp.Regexp, matchFunc func(string, []string, []string, *regexp.Regexp) []string,
-	noMatchFunc func([]string, *regexp.Regexp) []string) []string {
+func (t *TextProcessor) skip(i int, line string) bool {
+	if t.StartLine != -1 && i < t.StartLine {
+		return true
+	}
+	if t.EndLine != -1 && i > t.StartLine {
+		return true
+	}
+	results := t.AfterPattern.FindAllString(line, -1)
+	if results != nil {
+		t.inSearchArea = true
+	}
+	results = t.BeforePattern.FindAllString(line, -1)
+	if t.inSearchArea && results != nil {
+		t.inSearchArea = false
+		t.lastSearchAreaLine = i
+	}
+	return t.inSearchArea
+}
+
+func insertIntoLines(index int, line string, lines []string) []string {
+	var outputs []string
+	for i, l := range lines {
+		if index == i {
+			outputs = append(outputs, line)
+		}
+		outputs = append(outputs, l)
+	}
+	return outputs
+
+}
+
+func maxInt(v int, arr ...int) int {
+	max := v
+	for _, l := range arr {
+		if l > max {
+			max = l
+		}
+	}
+	return max
+}
+
+func (t TextProcessor) handleLines(lines []string, re *regexp.Regexp, matchFunc func(string, []string, []string, *regexp.Regexp) []string,
+	noMatchFunc func([]string, *regexp.Regexp, int) []string) []string {
 	var outputs []string
 	matched := false
-	for _, l := range lines {
-		results := re.FindAllString(l, -1)
-		if results != nil {
-			if matchFunc != nil {
-				outputs = matchFunc(l, results, outputs, re)
+	for i, l := range lines {
+
+		if t.skip(i, l) {
+			outputs = append(outputs, l)
+		} else {
+			results := re.FindAllString(l, -1)
+			if results != nil {
+				if matchFunc != nil {
+					outputs = matchFunc(l, results, outputs, re)
+				} else {
+					outputs = append(outputs, l)
+				}
+				matched = true
 			} else {
 				outputs = append(outputs, l)
 			}
-			matched = true
-		} else {
-			outputs = append(outputs, l)
 		}
 	}
 	if !matched && noMatchFunc != nil {
-		outputs = noMatchFunc(outputs, re)
+		p := maxInt(t.EndLine-1, t.lastSearchAreaLine)
+		if p == -1 {
+			p = len(outputs) - 1
+		}
+		outputs = noMatchFunc(outputs, re, p)
 	}
 	return outputs
 }
--- main.go
+++ main.go
@@ -23,12 +23,20 @@ func readFile(path string) string {
 
 func main() {
 	var args struct {
-		Path    string `arg:"positional"`
-		Pattern string `arg:"positional"`
-		Mode    string `arg:"-m"`
-		Text    string `arg:"-t"`
+		Path          string `arg:"positional"`
+		Pattern       string `arg:"positional"`
+		Mode          string `arg:"-m"`
+		Text          string `arg:"-t"`
+		StartLine     int
+		EndLine       int
+		BeforePattern string
+		AfterPattern  string
 	}
 	args.Text = ""
+	args.StartLine = -1
+	args.EndLine = -1
+	args.BeforePattern = ""
+	args.AfterPattern = ""
 	arg.MustParse(&args)
 
 	lines := strings.Split(readFile(args.Path), "\n")
@@ -41,19 +49,24 @@ func main() {
 
 	var outputs []string
 
+	p, err := lib.InitTextProcessor(args.StartLine, args.EndLine, args.BeforePattern, args.AfterPattern)
+
+	if err != nil {
+		panic(err)
+	}
 	switch args.Mode {
 	case "present":
-		outputs = lib.Present(lines, re, args.Pattern, args.Text)
+		outputs = p.Present(lines, re, args.Pattern, args.Text)
 	case "absent":
-		outputs = lib.Absent(lines, re)
+		outputs = p.Absent(lines, re)
 	case "insertAfter":
-		outputs = lib.InsertAfter(lines, re, args.Text)
+		outputs = p.InsertAfter(lines, re, args.Text)
 	case "insertBefore":
-		outputs = lib.InsertBefore(lines, re, args.Text)
+		outputs = p.InsertBefore(lines, re, args.Text)
 	case "replace":
-		outputs = lib.Replace(lines, re, args.Text)
+		outputs = p.Replace(lines, re, args.Text)
 	default:
-		outputs = lib.Present(lines, re, args.Pattern, args.Text)
+		outputs = p.Present(lines, re, args.Pattern, args.Text)
 
 	}
 
