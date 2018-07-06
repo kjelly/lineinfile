bb5564642f2268cb9e42a9206b148ebb80d6eb2d
Kuo-tung Kao <kjellytw@gmail.com>
2018-07-06 17:35:14 +0800

    Add more test

--- lib/replace_test.go
+++ lib/replace_test.go
@@ -26,7 +26,33 @@ uri=http://www.example.com
 driver=sql
 `
 	if o != output {
-		t.Errorf("Error %s, %s\n", o, sample)
+		t.Errorf("Error %s, %s\n", o, output)
+	}
+
+}
+
+func TestTextReplaceBetweenPattern(t *testing.T) {
+	pattern := "(user.*)"
+	p, _ := InitTextProcessor(pattern, "", "os_$1", -1, -1, "\\[auth\\]", "\\[data\\]")
+
+	o := strings.Join(p.Replace(strings.Split(sample, "\n")), "\n")
+
+	output := `
+[Default]
+user=abc
+password=abc
+url=http://www.example.com
+[auth]
+os_user=ccc
+password=ccc
+
+[data]
+url=http://example.com
+uri=http://www.example.com
+driver=sql
+`
+	if o != output {
+		t.Errorf("Error %s, %s\n", o, output)
 	}
 
 }
