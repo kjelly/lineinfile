fd00647420b9b6127da988041630efde6b127f85
kjelly <kjelllytw@gmail.com>
2018-12-18 22:53:31 +0800

    Add test code

--- lib/replace_test.go
+++ lib/replace_test.go
@@ -26,7 +26,67 @@ uri=http://www.example.com
 driver=sql
 `
 	if o != output {
-		t.Errorf("Error %s, %s\n", o, sample)
+		t.Errorf("Error %s, %s\n", o, output)
+	}
+
+}
+
+func TestTextReplaceBetweenPattern(t *testing.T) {
+	pattern := "(user.*)"
+	p, err := InitTextProcessor(pattern, "", "os_$1", -1, -1, "\\[auth\\]", "\\[data\\]")
+
+	if err != nil {
+		t.Errorf("%s\n", err)
+	}
+
+	o := strings.Join(p.Replace(strings.Split(sample, "\n")), "\n")
+
+	output := `
+[Default]
+user=abc
+password=abc
+url=http://www.example.com
+[auth]
+os_user=ccc
+password=ccc
+
+[data]
+url=http://example.com
+uri=http://www.example.com
+driver=sql
+`
+	if o != output {
+		t.Errorf("Error %s, %s\n", o, output)
+	}
+
+}
+
+func TestTextReplaceNotFoundBetweenPattern(t *testing.T) {
+	pattern := "(sql.*)"
+	p, err := InitTextProcessor(pattern, "", "os_$1", -1, -1, "\\[auth\\]", "\\[data\\]")
+
+	if err != nil {
+		t.Errorf("%s\n", err)
+	}
+
+	o := strings.Join(p.Replace(strings.Split(sample, "\n")), "\n")
+
+	output := `
+[Default]
+user=abc
+password=abc
+url=http://www.example.com
+[auth]
+user=ccc
+password=ccc
+
+[data]
+url=http://example.com
+uri=http://www.example.com
+driver=sql
+`
+	if o != output {
+		t.Errorf("Error %s, %s\n", o, output)
 	}
 
 }
